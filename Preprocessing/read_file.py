e43840d2cf89163d56649faf13bf4d53b1c995ac
michaelzhang <heronalps@gmail.com>
2018-05-11 19:23:04 -0700

    Add read_file.py

---
+++ Preprocessing/read_file.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from pathlib import Path
+
+#directory = "./dataset/Pro_41"
+
+def read(directory, block):
+    pathlist = Path(directory).glob('*')
+    frames = []
+
+    for path in pathlist :
+        print(str(path))
+        # Skip batch info rows
+        if str(path).endswith('csv') :
+            dataFrame = pd.read_csv(str(path), skiprows = 5)
+        elif str(path).endswith('xlsx'):
+            dataFrame = pd.read_excel(str(path), skiprows = 5)
+
+
+        # Drop bottom no Grader rows
+        dataFrame = dataFrame.dropna()
+
+        # Drop duplicate columns
+        dataFrame = dataFrame.loc[:, ~dataFrame.columns.duplicated()]
+
+        # Assign Block # to DataFrame
+        dataFrame = dataFrame.assign(block_Num = block)
+
+        # Append to the list of DataFrame
+        frames.append(dataFrame)
+
+    return pd.concat(frames)
+
+
+
+
+
