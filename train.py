532870d7000b7c42f6d4bd406e5f56b56cf9aa7b
michaelzhang <heronalps@gmail.com>
2018-05-27 15:09:52 -0700

    Modify .gitignore

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
 
+
 def train():
     # directory = ["Pro_41", "Pro_43", "Pro_98_80", "Pro_98_81W", "Pro_112_Clem_12-11-17", "Pro_112_Daisy_1-4-18", "Pro_112_Daisy_12-13-17"]
     # blocks = ["41N", "92E", "80", "81W", "64E", "64E", "64E"]
@@ -21,7 +22,7 @@ def train():
     # df = pd.concat(frames, sort=False)
 
     #df.to_csv("temp2.csv", index = False)
-    df = pd.read_csv("../temp2.csv")
+    df = pd.read_csv("./temp2.csv")
 
     # Dimensionality Reduction based on correlation analysis
     df = dim_reduce.reduce(df)
