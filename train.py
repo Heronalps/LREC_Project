423ecbe22692faa7db0f630623fb1a18c87be2ef
Michael Zhang <heronalps@gmail.com>
2018-06-11 09:04:58 -0700

    Update train.py

--- train.py
+++ train.py
@@ -9,19 +9,19 @@ from sklearn.metrics import classification_report
 
 
 def train():
-    # directory = ["Pro_41", "Pro_43", "Pro_98_80", "Pro_98_81W", "Pro_112_Clem_12-11-17", "Pro_112_Daisy_1-4-18", "Pro_112_Daisy_12-13-17"]
-    # blocks = ["41N", "92E", "80", "81W", "64E", "64E", "64E"]
-    # frames = []
-    # for i in range(len(directory)) :
-    #     print(directory[i])
-    #     temp_df = read_file.read("./dataset/" + directory[i], blocks[i])
-    #     frames.append(temp_df)
-    #
-    # # Aggregate dfs into one giant df
-    # df = pd.concat(frames, sort=False)
+    directory = ["Pro_41", "Pro_43", "Pro_98_80", "Pro_98_81W", "Pro_112_Clem_12-11-17", "Pro_112_Daisy_1-4-18", "Pro_112_Daisy_12-13-17"]
+    blocks = ["41N", "92E", "80", "81W", "64E", "64E", "64E"]
+    frames = []
+    for i in range(len(directory)) :
+        print(directory[i])
+        temp_df = read_file.read("./dataset/" + directory[i], blocks[i])
+        frames.append(temp_df)
+    
+    # Aggregate dfs into one giant df
+    df = pd.concat(frames, sort=False)
 
-    #df.to_csv("temp2.csv", index = False)
-    df = pd.read_csv("./temp2.csv")
+    # #df.to_csv("temp2.csv", index = False)
+    # df = pd.read_csv("./temp2.csv")
 
     # Dimensionality Reduction based on correlation analysis
     df = reduce(df)
