6b1cbcb43a31ca34145842876d7dd81238538a18
Michael Zhang <heronalps@gmail.com>
2018-12-23 10:52:29 -0800

    mz - replace Imputer by SimpleImputer

--- lambda/XGBoost.py
+++ lambda/XGBoost.py
@@ -2,11 +2,10 @@ import pandas as pd
 import numpy as np
 import os, boto3
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import Imputer
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 from xgboost import XGBRegressor
-# from subprocess import check_output
 
 local_repo = os.path.join(os.path.sep, "tmp", os.path.basename('csv'))
 client = boto3.client('s3')
@@ -30,7 +29,7 @@ def lambda_handler(event, context):
     train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.25)
 
     # This Imputer uses the default strategy as mean
-    my_imputer = Imputer()
+    my_imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
     train_X = my_imputer.fit_transform(train_X)
     test_X = my_imputer.transform(test_X)
 
@@ -48,7 +47,8 @@ def lambda_handler(event, context):
 
     from sklearn.metrics import mean_absolute_error
     print("Mean Absolute Error : " + str(mean_absolute_error(predictions, test_y)))
-
-    np.unique(test_y, return_counts=True)
-    np.unique(predictions.round(), return_counts=True)
+    print("====================")
+    print(np.unique(test_y, return_counts=True))
+    print(np.unique(predictions.round(), return_counts=True))
+    print("====================")
     print("Accuracy Score: " + str(accuracy_score(test_y, predictions.round(), normalize=True)))
